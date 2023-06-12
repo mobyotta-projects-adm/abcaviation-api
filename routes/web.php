959689466905294e8877657c3148026d07287727
Rohit Nishad <r.n.rohitnishad@gmail.com>
2023-06-12 19:29:53 +0530

    ui routes

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,16 @@ Route::get('/newsfeed', function () {    return view('user.newsfeed'); });
 Route::get('/tasks', function () {    return view('user.tasks'); });
 Route::get('/locations', function () {    return view('user.locations'); });
 Route::get('/team', function () {    return view('user.team'); });
+Route::get('/me', function () {
+    return view('dashboard');
+})->name('dashboard');
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified'
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
