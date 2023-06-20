fb92515615031a77c2e40590c3cb221eed5c76a0
Rohit Nishad <r.n.rohitnishad@gmail.com>
2023-06-20 18:36:59 +0530

    Add auth to routes

--- routes/web.php
+++ routes/web.php
@@ -20,29 +20,26 @@ Route::get('/', function () {
     return redirect('login');
 });
 
-// Route::middleware([
-//     'auth:sanctum',
-//     config('jetstream.auth_session'),
-//     'verified'
-// ])->group(function () {
-//     Route::get('/dashboard', function () {
-//         return view('dashboard');
-//     })->name('dashboard');
-// });
-
-Route::get('/dashboard', function () {
-    return redirect('me');
-})->name('dashboard');
-
-Route::get('me', [DashboardController::class, 'me'])->name('me');
-Route::get('newsfeed', [DashboardController::class, 'newsfeed'])->name('newsfeed');
-Route::get('tasks', [DashboardController::class, 'tasks'])->name('tasks');
-Route::get('location', LocationTable::class)->name('location');
-Route::get('people', [DashboardController::class, 'people'])->name('people');
-Route::get('schedule', [DashboardController::class, 'schedule'])->name('schedule');
-Route::get('approved-timesheet', [DashboardController::class, 'approved_timesheet'])->name('approved_timesheet');
-Route::get('export-timesheet', [DashboardController::class, 'export_timesheet'])->name('export_timesheet');
-Route::get('report', [DashboardController::class, 'report'])->name('report');
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
 
 
-Route::get('/location/edit/{location}', EditLocation::class)->name('editlocation');
+
+    Route::get('/dashboard', function () {
+        return redirect('me');
+    })->name('dashboard');
+
+    Route::get('me', [DashboardController::class, 'me'])->name('me');
+    Route::get('newsfeed', [DashboardController::class, 'newsfeed'])->name('newsfeed');
+    Route::get('tasks', [DashboardController::class, 'tasks'])->name('tasks');
+    Route::get('location', LocationTable::class)->name('location');
+    Route::get('people', [DashboardController::class, 'people'])->name('people');
+    Route::get('schedule', [DashboardController::class, 'schedule'])->name('schedule');
+    Route::get('approved-timesheet', [DashboardController::class, 'approved_timesheet'])->name('approved_timesheet');
+    Route::get('export-timesheet', [DashboardController::class, 'export_timesheet'])->name('export_timesheet');
+    Route::get('report', [DashboardController::class, 'report'])->name('report');
+    Route::get('/location/edit/{location}', EditLocation::class)->name('editlocation');
+});
