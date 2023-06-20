4507858b72f2427833ceabc7839416aee32d976a
Rohit Nishad <r.n.rohitnishad@gmail.com>
2023-06-20 15:49:46 +0530

    Tasks

--- resources/views/pages/tasks.blade.php
+++ resources/views/pages/tasks.blade.php
@@ -4,17 +4,129 @@
         <div class="col-sm-12">
             <div class="card">
                 <div class="card-header">
-                    <h5>Tasks card</h5>
+                    <h5>Tasks</h5>
                 </div>
                 <div class="card-body">
-                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                        incididunt ut labore et dolore magna
-                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat. Duis
-                        aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                    </p>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="card">
+                                <div class="card-body pb-0">
+                                    <div class="d-flex align-items-center justify-content-between">
+                                        <h5 class="mb-0">My Task</h5>
+                                        <div class="dropdown">
+                                            <a class="avtar avtar-s btn-link-secondary dropdown-toggle arrow-none"
+                                                href="#" data-bs-toggle="dropdown" aria-haspopup="true"
+                                                aria-expanded="false">
+                                                <i class="ti ti-dots-vertical f-18"></i>
+                                            </a>
+                                            <div class="dropdown-menu dropdown-menu-end">
+                                                <a class="dropdown-item" href="#">Today</a>
+                                                <a class="dropdown-item" href="#">Weekly</a>
+                                                <a class="dropdown-item" href="#">Monthly</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <ul class="list-group list-group-flush border-top-0">
+                                    <li class="list-group-item">
+                                        <div class="d-flex align-items-start">
+                                            <div class="flex-grow-1 me-2">
+                                                <h6 class="mb-0">Follow up client for feedback</h6>
+                                                <p class="my-1"><i class="ti ti-archive"></i> Sending report</p>
+                                                <span class="badge bg-danger rounded-pill">00 : 15</span>
+                                            </div>
+                                            <div class="flex-shrink-0">
+                                                <a href="#" class="avtar avtar-s btn-link-secondary">
+                                                    <i class="ti ti-circle-check text-success f-18"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="list-group-item">
+                                        <div class="d-flex align-items-start">
+                                            <div class="flex-grow-1 me-2">
+                                                <h6 class="mb-0">Follow up client for feedback</h6>
+                                                <p class="my-1"><i class="ti ti-folder"></i> Received report</p>
+                                                <span class="badge bg-success rounded-pill">00 : 30</span>
+                                            </div>
+                                            <div class="flex-shrink-0">
+                                                <a href="#" class="avtar avtar-s btn-link-secondary">
+                                                    <i class="ti ti-circle-check f-18"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="list-group-item">
+                                        <div class="d-flex align-items-start">
+                                            <div class="flex-grow-1 me-2">
+                                                <h6 class="mb-0">Follow up client for feedback</h6>
+                                                <p class="my-1"><i class="ti ti-archive"></i> Sending report</p>
+                                                <span class="badge bg-danger rounded-pill">00 : 15</span>
+                                            </div>
+                                            <div class="flex-shrink-0">
+                                                <a href="#" class="avtar avtar-s btn-link-secondary">
+                                                    <i class="ti ti-circle-check f-18"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+
+                        <div class="col-md-6">
+                            <div class="card task-card">
+                              <div class="card-header d-flex align-items-center justify-content-between">
+                                <h5>Tasks</h5>
+                                <a href="#" class="link-primary">View all</a>
+                              </div>
+                              <div class="card-body">
+                                <ul class="list-unstyled task-list">
+                                  <li>
+                                    <i class="feather icon-check f-w-600 task-icon bg-success"></i>
+                                    <p class="m-b-5">8:50</p>
+                                    <h5 class="text-muted"
+                                      >Call to customer <span class="text-primary"> <a href="#!" class="text-primary">Jacob</a> </span> and discuss the</h5
+                                    >
+                                  </li>
+                                  <li>
+                                    <i class="task-icon bg-primary"></i>
+                                    <p class="m-b-5">Sat, 5 Mar</p>
+                                    <h5 class="text-muted">Design mobile Application</h5>
+                                  </li>
+                                  <li>
+                                    <i class="task-icon bg-danger"></i>
+                                    <p class="m-b-5">Sun, 17 Feb</p>
+                                    <h5 class="text-muted"
+                                      ><span class="text-primary"><a href="#!" class="text-primary">Jeny</a></span>
+                                      assign you a task
+                                      <span class="text-primary"><a href="#!" class="text-primary">Mockup Design.</a></span>
+                                    </h5>
+                                  </li>
+                                  <li>
+                                    <i class="task-icon bg-warning"></i>
+                                    <p class="m-b-5">Sat, 18 Mar</p>
+                                    <h5 class="text-muted">Design logo</h5>
+                                  </li>
+                                  <li class="p-b-15 m-b-10">
+                                    <i class="task-icon bg-success"></i>
+                                    <p class="m-b-5">Sat, 22 Mar</p>
+                                    <h5 class="text-muted">Design mobile Application</h5>
+                                  </li>
+                                </ul>
+                                <div class="text-end">
+                                  <a href="#!" class="b-b-primary text-primary">View Friend List</a>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+
+
+
+
+                    </div>
+
                 </div>
             </div>
         </div>
