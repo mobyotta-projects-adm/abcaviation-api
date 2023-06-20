69c8cba38ab2e2506e452e00039aa433749050d9
Rohit Nishad <r.n.rohitnishad@gmail.com>
2023-06-20 11:31:15 +0530

    Navigatiion menu

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    //
+
+    public function me()
+    {
+        return view('pages.me');
+    }
+    public function newsfeed()
+    {
+        return view('pages.newsfeed');
+    }
+    public function tasks()
+    {
+        return view('pages.tasks');
+    }
+    public function location()
+    {
+        return view('pages.locations');
+    }
+    public function people()
+    {
+        return view('pages.people');
+    }
+    public function schedule()
+    {
+        return view('pages.shedule');
+    }
+    public function approved_timesheet()
+    {
+        return view('pages.approve-timesheets');
+    }
+    public function export_timesheet()
+    {
+        return view('pages.export-timesheet');
+    }
+    public function report()
+    {
+        return view('pages.report');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,3 +31,13 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::get('me', [DashboardController::class, 'me'])->name('me');
+Route::get('newsfeed', [DashboardController::class, 'newsfeed'])->name('newsfeed');
+Route::get('tasks', [DashboardController::class, 'tasks'])->name('tasks');
+Route::get('location', [DashboardController::class, 'location'])->name('location');
+Route::get('people', [DashboardController::class, 'people'])->name('people');
+Route::get('schedule', [DashboardController::class, 'schedule'])->name('schedule');
+Route::get('approved-timesheet', [DashboardController::class, 'approved_timesheet'])->name('approved_timesheet');
+Route::get('export-timesheet', [DashboardController::class, 'export_timesheet'])->name('export_timesheet');
+Route::get('report', [DashboardController::class, 'report'])->name('report');
